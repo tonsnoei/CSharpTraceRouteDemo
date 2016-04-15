e80fa7c9800532f40c7e18aa936dae1c902201d5
Ton Snoei <ton@snoei.net>
2016-04-15 18:55:52 +0200

    First version of the TraceRoute WinForms Application.

---
+++ TraceRouteWinForms/frmTraceRoute.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TraceRouteWinForms
+{
+    partial class frmTraceRoute
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Trace Route";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "google.nl";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hostname / IP Address:";
+            // 
+            // frmTraceRoute
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 268);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmTraceRoute";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SNOEI.NET Trace Route";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TraceRouteWinForms/frmTraceRoute.cs
@@ -0,0 +1,112 @@
+﻿/*
+
+ MIT License
+
+ Copyright (c) 2016, SNOEI.NET (Ton Snoei)
+
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TraceRouteWinForms
+{
+    public partial class frmTraceRoute : Form
+    {
+        public frmTraceRoute()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            await Task.Run(() => TraceRouteTask(30, textBox1.Text));
+        }
+
+        private void TraceRouteTask(int hopLimit, string hostName)
+        {
+            List<TraceRouteHopInfo> hops = TraceRoute(hopLimit, hostName);
+        }
+
+        private List<TraceRouteHopInfo> TraceRoute(int hopLimit, string hostName)
+        {
+            List<TraceRouteHopInfo> result = new List<TraceRouteHopInfo>();
+
+            bool dontFragment = true;
+            string data = Guid.NewGuid().ToString();
+
+            for (int hopIndex = 0; hopIndex < hopLimit; hopIndex++)
+            {
+                // Setting the TTL is the heart of the traceroute principle
+                int ttl = hopIndex + 1;
+                int timeout = 15000; // 15 seconds.
+                byte[] dataBytes = Encoding.ASCII.GetBytes(data);
+                Ping ping = new Ping();
+                PingReply pingReply = default(PingReply);
+                PingOptions pingOptions = new PingOptions(ttl, dontFragment);
+
+                // Let's ping
+                pingReply = ping.Send(hostName, timeout, dataBytes, pingOptions);
+
+                TraceRouteHopInfo traceRouteHopInfo = new TraceRouteHopInfo() { HopIndex = hopIndex+1, PingReply = pingReply };
+                result.Add(traceRouteHopInfo);
+
+                UpdateUI(traceRouteHopInfo);
+
+                if (pingReply.Status == IPStatus.Success)
+                    // The ping reached the destination after all hops in between.
+                    break;
+            }
+
+            return result;
+        }
+
+        private void UpdateUI(TraceRouteHopInfo traceRouteHopInfo)
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new Action<TraceRouteHopInfo>(UpdateUI), traceRouteHopInfo);
+            else
+            {
+                listBox1.Items.Add(string.Format("{0}. {1}",
+                    traceRouteHopInfo.HopIndex,
+                    traceRouteHopInfo.PingReply.Address.ToString()));
+            }
+
+        }
+    }
+
+
+
+    public class TraceRouteHopInfo
+    {
+        public int  HopIndex { get; set; }
+        public PingReply PingReply { get; set; }
+    }
+}
